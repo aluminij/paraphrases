f25e77fc91181841a5aa716b0c765877c8b40898
aljad <alja.debeljak@gmail.com>
2023-01-22 09:32:04 +0100

    add .json dumping

--- x.py
+++ x.py
@@ -1,4 +1,5 @@
 import deepl
+import json
 import os
 import pandas as pd
 from dotenv import load_dotenv
@@ -10,10 +11,8 @@ translator = deepl.Translator(auth_key)
 eng = pd.read_csv("aligned/en_ab.txt", header=None, sep='\n')
 orig = pd.read_csv("aligned/sl_ab.txt", header=None, encoding="utf-8", sep='\n')
 para_dict = dict()
-for i, row in eng.loc[:100,:].iterrows():
+for i, row in eng.loc[:5,:].iterrows():
     para_dict[orig.loc[i,0]] = translator.translate_text(row[0], source_lang="EN", target_lang="SL").text
-
-print(result.text)  # "Bonjour, le monde !"
-
-
+with open("paras.json", "w") as f:
+    json.dump(para_dict, f)
 pass
