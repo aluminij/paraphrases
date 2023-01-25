ac51c4e9441f5d1a26194829baa40f5f9b61bae4
aljad <alja.debeljak@gmail.com>
2023-01-25 21:19:23 +0100

    add dvc tracking

--- x.py
+++ x.py
@@ -2,29 +2,32 @@ import deepl
 import json
 import os
 import pandas as pd
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
 translator = deepl.Translator(os.getenv("AUTH_KEY"))
 
-def create_dict(f_name: str) -> dict[str, dict]:
+def create_df(f_name: str) -> pd.DataFrame:
     """
-    Takes file prefix to read aligned slovene and english files.
-    Creates and returns dictionary where keys are slovene source phrases, values are
-    dictionary with english source phrases and their translations.
+    Takes file suffix to read aligned slovene and english files.
+    Creates and returns dataframe with the slovene source phrase, english source phrase and its slovene translation.
     """
     sl_f = pd.read_csv(f"aligned/sl_{f_name}.txt", header=None, sep="\n", encoding="utf-8")
     en_f = pd.read_csv(f"aligned/en_{f_name}.txt", header=None, sep="\n")
-    para_dict = dict()
-    for i, row in en_f.loc[:10, :].iterrows():
+    para_df = pd.DataFrame(index=[0], columns=["sl_source", "en_source", "sl_trans"])
+    for i, row in en_f.iterrows():
         sl_trans = translator.translate_text(row[0], source_lang="EN", target_lang="SL").text
-        para_dict[i] = {"sl_source": sl_f.loc[i, 0].strip(), "eng_source": row[0].strip(), "sl_trans": sl_trans}
-    return para_dict
+        para_df.iloc[len(para_df.index)] = [sl_f.loc[i, 0], row[0], sl_trans]
+    return para_df 
 
-f_prefix = "ab"
-para_dict = create_dict(f_prefix)
+
+f_suffix = "ah"
+para_df = create_df(f_suffix)
+para_df_clean = para_df.applymap(lambda x: re.sub("^-", "", x).strip())
 # remove entries with identical translations
-para_dict2 = {k: v for k, v in para_dict.items() if k != v["sl_trans"]}
-with open(f"interim/paras_{f_prefix}.json", "w", encoding="utf-8") as f:
-    json.dump(para_dict, f)
+para_df_unique = para_df_clean.loc[para_df_clean["sl_source"] != para_df_clean["sl_trans"], :]
+with open(f"interim/paras_{f_suffix}.json", "w", encoding="utf-8") as f:
+    json.dump(para_df_unique, f)
+
 
