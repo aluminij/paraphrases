cf98aa84a8b3ee18b5021c21caaed7893d6a9bcd
aljad <alja.debeljak@gmail.com>
2023-02-04 14:14:22 +0100

    fixed separator

--- paraphrase.py
+++ paraphrase.py
@@ -13,16 +13,17 @@ def create_trans_df(f_name: str) -> pd.DataFrame:
     Takes file suffix to read aligned slovene and english files.
     Creates and returns dataframe with the slovene source phrase, english source phrase and its slovene translation.
     """
-    sl_f = pd.read_csv(f"aligned/sl_{f_name}.txt", header=None, sep="\n", encoding="utf-8")
-    en_f = pd.read_csv(f"aligned/en_{f_name}.txt", header=None, sep="\n")
+    sl_f = pd.read_csv(f"aligned/sl_{f_name}.txt", header=None, sep="\n|\r\n", encoding="utf-8")
+    en_f = pd.read_csv(f"aligned/en_{f_name}.txt", header=None, sep="\n|\r\n")
     para_df = pd.DataFrame(index=[0], columns=["sl_source", "en_source", "sl_trans"])
+
     for i, row in en_f.iterrows():
         sl_trans = translator.translate_text(row[0], source_lang="EN", target_lang="SL").text
         para_df.loc[i] = [sl_f.loc[i, 0], row[0], sl_trans]
     return para_df 
 
 
-f_suffix = "ct"
+f_suffix = "zh"
 para_df = create_trans_df(f_suffix)
 para_df_clean = para_df.applymap(lambda x: re.sub("^-", "", x).strip())
 # remove entries with identical translations
