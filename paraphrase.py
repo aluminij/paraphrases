7778e6ae1473a1a74ff51aa99594ee64b8b3146f
aljad <alja.debeljak@gmail.com>
2023-02-06 17:42:27 +0100

    added parsing function

--- paraphrase.py
+++ paraphrase.py
@@ -8,6 +8,26 @@ from dotenv import load_dotenv
 load_dotenv()
 translator = deepl.Translator(os.getenv("AUTH_KEY"))
 
+def read_aligned(f_name: str, lang: str="en") -> pd.DataFrame:
+    if lang == "en":
+        df = pd.read_csv(f"aligned/en_{f_name}.txt", header=None, sep="\n|\r\n")
+    elif lang == "sl":
+        df = pd.read_csv(f"aligned/sl_{f_name}.txt", header=None, sep="\n|\r\n", encoding="utf-8")
+    return df
+
+def split_text(f_name: str, lang: str="en") -> pd.DataFrame:
+    df = read_aligned(f_name, lang)
+    df_split = pd.DataFrame(columns=["line"])
+    j = 0
+    for i, row in df.iterrows():
+        line_split = re.split(r'(?<=[\.\!\?])\s', row[0])
+        df_split.loc[j, "line"] = line_split[0]
+        j += 1
+        for l in line_split[1:]:
+            df_split.loc[j, "line"] = l
+            j += 1
+    return df_split
+        
 def create_trans_df(f_name: str) -> pd.DataFrame:
     """
     Takes file suffix to read aligned slovene and english files.
@@ -23,7 +43,10 @@ def create_trans_df(f_name: str) -> pd.DataFrame:
     return para_df 
 
 
-f_suffix = "zh"
+f_suffix = "ut"
+df_split = split_text(f_suffix)
+
+
 para_df = create_trans_df(f_suffix)
 para_df_clean = para_df.applymap(lambda x: re.sub("^-", "", x).strip())
 # remove entries with identical translations
