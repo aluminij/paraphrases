050bc2365dcb7fbb1ef8f122f4ef181a97d57881
aljad <alja.debeljak@gmail.com>
2024-03-13 17:15:32 +0100

    generate paraphrases

--- src/generate_seq2seq.py
+++ src/generate_seq2seq.py
@@ -1,50 +1,63 @@
 import evaluate
+import pandas as pd
 from datasets import load_dataset
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
+data_files = {
+    "train": "paired_1_datasets/train.jl",
+    "val": "paired_1_datasets/eval.jl",
+    "test": "paired_1_datasets/test.jl"
+}
+new_para = load_dataset('json', data_files=data_files)
 
-if __name__ == "__main__":
-    data_files = {
-        "train": "train_midi.jsonl",
-        "val": "eval_midi.jsonl",
-        "test": "test_midi.jsonl"
-    }
-    new_para = load_dataset('json', data_files=data_files)
+metric_btscore = evaluate.load("bertscore")
+metric_rouge = evaluate.load("rouge")
 
-    metric = evaluate.load("bertscore")
+model_path = "src/paraphrase/model/t5-sl-small-paired1-whole-mbert"
+tokenizer = T5Tokenizer.from_pretrained(model_path)
+model = T5ForConditionalGeneration.from_pretrained(model_path)
+prefix = "paraphrase: "
 
-    model_path = "./model/t5-sl-small-midi-data-bert"
+gen_text = pd.DataFrame()
 
-    # Use the model you trained here, or something pre-trained from https://huggingface.co/models
-    tokenizer = T5Tokenizer.from_pretrained(model_path)
+def generate_para(orig: str) -> str:
+    input_ids = tokenizer(f"paraphrase {orig}", return_tensors="pt", max_length=128, truncation=True).input_ids
+    outputs = model.generate(input_ids,
+                                max_length=128,
+                                num_beams=5,
+                                do_sample=True,
+                                top_k=5,
+                                temperature=0.4
+                                )
 
-    model = T5ForConditionalGeneration.from_pretrained(model_path)
+    decoded_preds = [tokenizer.decode(outputs[0].cpu(), skip_special_tokens=True)]
+    return decoded_preds
 
-    prefix = "paraphrase: "
 
-    for _i in range(len(new_para["test"])):
-        sent = new_para["test"][_i]["phrase"]
-        print("SOURCE:\t", sent)
+for _i in range(len(new_para["val"])):
+    sent = new_para["val"][_i]["phrase"]
+    gen_text.loc[_i, "source"] = sent
 
-        decoded_labels = [new_para["test"][_i]["paraphrase"]]
-        print("TARGET:\t", decoded_labels)
+    decoded_labels = [new_para["val"][_i]["paraphrase"]]
+    gen_text.loc[_i, "target"] = decoded_labels
+    
+    input_ids = tokenizer(f"{prefix} {sent}", return_tensors="pt", max_length=128, truncation=True).input_ids
+    outputs = model.generate(input_ids,
+                                max_length=128,
+                                num_beams=5,
+                                do_sample=True,
+                                top_k=5,
+                                temperature=0.7
+                                )
 
-        input_ids = tokenizer(f"{prefix} {sent}", return_tensors="pt", max_length=128, truncation=True).input_ids
-        # Tweak generation parameters to increase/decrease diversity of generated text
-        # See https://huggingface.co/docs/transformers/v4.25.1/en/main_classes/text_generation#transformers.GenerationMixin.generate
-        outputs = model.generate(input_ids,
-                                 max_length=128,
-                                 num_beams=5,
-                                 do_sample=True,
-                                 top_k=5,
-                                 temperature=0.7
-                                 )
+    decoded_preds = [tokenizer.decode(outputs[0].cpu(), skip_special_tokens=True)]
+    gen_text.loc[_i, "prediction"] = decoded_preds
 
-        decoded_preds = [tokenizer.decode(outputs[0].cpu(), skip_special_tokens=True)]
-        print("PREDICTION:\t", decoded_preds, "\n")
-
-        # Note that this is the example-level metric (as opposed to dataset-level metric)
-        result = metric.compute(predictions=decoded_preds, references=decoded_labels, lang="sl")
-
-        print(result)
-        print("\n")
+    result_btsc = metric_btscore.compute(predictions=decoded_preds, references=decoded_labels, lang="sl")
+    result_rouge = metric_rouge.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True, rouge_types=['rouge1', 'rouge2', 'rougeL'])
+    print(f"original: {sent} \npara: {decoded_labels[0]} \npredict: {decoded_preds[0]}" )
+    for m in ["precision", "recall", "f1"]:
+        gen_text.loc[_i, f"bertscore_{m}"] = result_btsc[m]
+    for m2 in ["1","2", "L" ]:
+        gen_text.loc[_i, f"rouge{m2}"] = result_rouge[f"rouge{m2}"]
+gen_text.to_excel("eval_paras.xlsx")
