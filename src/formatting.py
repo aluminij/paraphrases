fc884ef913771b82a1e05407868f1d96a0ec4cd9
aljad <alja.debeljak@gmail.com>
2024-11-14 16:45:51 +0100

    fix formatting functions

--- src/formatting.py
+++ src/formatting.py
@@ -1,5 +1,6 @@
-import itertools
 import json
+import jsonlines
+import os
 import pandas as pd
 import re
 
@@ -19,34 +20,25 @@ def paraphrase_check(p_list: list, p0: str) -> bool:
             return False
     return True        
 
-def read_aligned(f_name: str, lang: str="en") -> pd.DataFrame:
-    """Reads aligned data into dataframe."""
-    if lang == "en":
-        df = pd.read_csv(f"aligned/en_{f_name}.txt", header=None, sep="\n|\r\n")
-    elif lang == "sl":
-        df = pd.read_csv(f"aligned/sl_{f_name}.txt", header=None, sep="\n|\r\n", encoding="utf-8")
-    return df
-
 def split_text(f_name: str, lang: str="en") -> pd.DataFrame:
     """
     Takes file suffix of text file. 
     Handles different punctuation marks. 
     If there's an end mark inside of a row, it shifts the remainder to the next row.
-    Ideally in the end every row represents a sentence.
-    #TODO: exclude Mr., Mrs. and such special cases. 
+    Ideally in the end every row represents a single sentence.
     """
-    df = read_aligned(f_name, lang)
+    df = pd.read_csv(f"data/aligned/{lang}_{f_name}.txt", header=None, sep="\n|\r\n")
     df_clean = df.apply(lambda x: x.replace(r'^\s*\-*\s*', '', regex=True).replace(r"\.{2,}\s*$", ".", regex=True).replace(r"\.{2,}", '', regex=True))
     df_split = pd.DataFrame(columns=["line"])
     j = 0
-    for i, row in df_clean.iterrows():
+    for _, row in df_clean.iterrows():
         line_split = re.split(r'(?<=[\.\!\?])\s', row[0])
         df_split.loc[j, "line"] = line_split[0]
         j += 1
         for l in line_split[1:]:
             df_split.loc[j, "line"] = l
             j += 1
-    df_split.to_csv(f"formatted/{lang}_{f_name}_fmt.txt", header=None, index=None, mode="a")
+    df_split.to_csv(f"data/formatted/{lang}_{f_name}_fmt.txt", header=None, index=None, mode="a")
     return
 
 def slice_files(f_name: str, batch_size: int=100):
@@ -69,36 +61,23 @@ def slice_files(f_name: str, batch_size: int=100):
                     sliced = True
                 json.dump(df.to_dict(orient="index"), sliced_f, ensure_ascii=False)
                 i += 1
-    
-def pair_sl_paraphrases() -> pd.DataFrame:
-    """Takes paraphrase batches from json file and sorts them into pairs which are needed for training data."""
-    df_slo_paras = pd.DataFrame(columns=["phrase", "paraphrase"])
-    with open("done_part2.json", encoding="utf-8") as json_file:
-        d = json.load(json_file)
-    i = 0
-    for _, v in d.items():
-        para_list = [val for k, val in v.items() if ("sl" in k) and not val is None]
-        for combo in itertools.permutations(para_list, 2):
-            df_slo_paras.loc[i, :] = [combo[0], combo[1]]
-            i += 1
-    return df_slo_paras
 
-
-def remove_similar_phrases(path: str):
-    """Removes phrases with more than 70% word matching."""
-    with open(path, "r", encoding="utf-8") as f:
-        filtered_path = path.split("/")
-        filtered_path = f"{filtered_path[0]}/filtered_{filtered_path[1]}"
-        with open(filtered_path, 'w', encoding="utf-8") as outfile:
-            i = 0
-            new_d = {}
-            for line in f:
-                line_dict = json.loads(line)
-                p1 = list(itertools.chain.from_iterable([l.split() for l in line_dict["phrase"].split(",")]))
-                p2 = list(itertools.chain.from_iterable([l.split() for l in line_dict["paraphrase"].split(",")]))
-                intersect = set(p1).intersection(set(p2))
-                if int(0.7*max(len(p1), len(p2))) >= len(intersect):
-                    new_d[str(i)] = line_dict
-                    json.dump(line_dict, outfile, ensure_ascii=False)
-                    outfile.write('\n')
-                    i += 1
+def filter_phrases(path: str):
+    """Looks through .jsonl files in the path folder, removes phrases with more than 70% word matching,
+    saves into separate "filtered" folder in the form of "{filename}_filtered.jsonl" file.
+    New version is formatted the same, only removes phrases that have >70% matching with a different phrase."""
+    data = os.listdir(os.getcwd()+"\\"+path)
+    for d in data:
+        if len(d.split(".")) == 2: #looking for files, not folders    
+            para_list = []
+            d_path_name = d.split(".")[0]
+            with open("\\".join((path, d)), "r", encoding="utf-8") as f:
+                for i, line in enumerate(f):
+                        line_dict = json.loads(line)
+                        p1 = line_dict["phrase"].split()
+                        p2 = line_dict["paraphrase"].split()
+                        intersect = set(p1).intersection(set(p2))
+                        if int(0.7*max(len(p1), len(p2))) >= len(intersect):
+                            para_list.append(line_dict)
+            with jsonlines.open("/".join((path, "filtered", f"{d_path_name}_f.jsonl")), 'w') as outfile:
+                outfile.write_all(para_list)
