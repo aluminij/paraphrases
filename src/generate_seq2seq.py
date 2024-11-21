f4b3fc806263c525f19d5e34c0e4603d17e607d9
aljad <alja.debeljak@gmail.com>
2024-11-21 12:34:49 +0100

    add generate scripts

--- src/generate_seq2seq.py
+++ src/generate_seq2seq.py
@@ -1,63 +1,66 @@
+import code_bert_score
 import evaluate
 import pandas as pd
-from datasets import load_dataset
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import MT5ForConditionalGeneration, MT5Tokenizer
 
-data_files = {
-    "train": "paired_1_datasets/train.jl",
-    "val": "paired_1_datasets/eval.jl",
-    "test": "paired_1_datasets/test.jl"
-}
-new_para = load_dataset('json', data_files=data_files)
+from utils.lemma_rouge import custom_rouge
+
+df = pd.read_json("data/test_largest.jsonl", lines=True)
 
 metric_btscore = evaluate.load("bertscore")
-metric_rouge = evaluate.load("rouge")
 
-model_path = "src/paraphrase/model/t5-sl-small-paired1-whole-mbert"
-tokenizer = T5Tokenizer.from_pretrained(model_path)
-model = T5ForConditionalGeneration.from_pretrained(model_path)
-prefix = "paraphrase: "
+model_path = "src/paraphrase/model/mT5-largest-sloberta"
+tokenizer = MT5Tokenizer.from_pretrained(model_path)
+model = MT5ForConditionalGeneration.from_pretrained(model_path)
+prefix = "generiraj parafrazo: "
 
 gen_text = pd.DataFrame()
 
-def generate_para(orig: str) -> str:
-    input_ids = tokenizer(f"paraphrase {orig}", return_tensors="pt", max_length=128, truncation=True).input_ids
-    outputs = model.generate(input_ids,
-                                max_length=128,
-                                num_beams=5,
-                                do_sample=True,
-                                top_k=5,
-                                temperature=0.4
-                                )
 
-    decoded_preds = [tokenizer.decode(outputs[0].cpu(), skip_special_tokens=True)]
-    return decoded_preds
+def choose_para(sent: str, i_repeat: int) -> str:
+    input_ids = tokenizer(
+        f"{prefix} {sent}", return_tensors="pt", max_length=128
+    ).input_ids
+
+    def generate_para(input_ids, i: int) -> list[str]:
+        temp = 0.8 + 0.1 * i
+        outputs = model.generate(
+            input_ids,
+            max_length=128,
+            num_beams=5,
+            do_sample=True,
+            top_p=0.9,
+            no_repeat_ngram_size=2,
+            temperature=temp,
+            num_return_sequences=3,
+        )
+
+        decoded_preds = [
+            tokenizer.decode(o.cpu(), skip_special_tokens=True) for o in outputs
+        ]
+        return decoded_preds
+
+    for i_repeat in range(10):
+        decoded_preds = generate_para(input_ids, i_repeat)
+        s = pd.Series(decoded_preds).value_counts()
+        top_s = [x for x in s.index.to_list() if x != sent]
+        if len(top_s) > 0:
+            break
+    if len(top_s) == 0:
+        top_s = [sent]
+    return top_s[0]
 
 
-for _i in range(len(new_para["val"])):
-    sent = new_para["val"][_i]["phrase"]
-    gen_text.loc[_i, "source"] = sent
-
-    decoded_labels = [new_para["val"][_i]["paraphrase"]]
-    gen_text.loc[_i, "target"] = decoded_labels
-    
-    input_ids = tokenizer(f"{prefix} {sent}", return_tensors="pt", max_length=128, truncation=True).input_ids
-    outputs = model.generate(input_ids,
-                                max_length=128,
-                                num_beams=5,
-                                do_sample=True,
-                                top_k=5,
-                                temperature=0.7
-                                )
-
-    decoded_preds = [tokenizer.decode(outputs[0].cpu(), skip_special_tokens=True)]
-    gen_text.loc[_i, "prediction"] = decoded_preds
-
-    result_btsc = metric_btscore.compute(predictions=decoded_preds, references=decoded_labels, lang="sl")
-    result_rouge = metric_rouge.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True, rouge_types=['rouge1', 'rouge2', 'rougeL'])
-    print(f"original: {sent} \npara: {decoded_labels[0]} \npredict: {decoded_preds[0]}" )
-    for m in ["precision", "recall", "f1"]:
-        gen_text.loc[_i, f"bertscore_{m}"] = result_btsc[m]
-    for m2 in ["1","2", "L" ]:
-        gen_text.loc[_i, f"rouge{m2}"] = result_rouge[f"rouge{m2}"]
-gen_text.to_excel("eval_paras.xlsx")
+df["prediction"] = df["phrase"].apply(lambda x: choose_para(x, 10))
+df["bertscore"] = metric_btscore.compute(
+    predictions=df["prediction"], references=df["phrase"], lang="sl"
+)["f1"]
+df["sloberta"] = code_bert_score.score(
+    cands=df["prediction"],
+    refs=df["phrase"],
+    model_type="EMBEDDIA/sloberta",
+)[2].tolist()
+df["rouge"] = df.apply(
+    lambda x: custom_rouge(ref=x["phrase"], gen=x["prediction"])["rougeL"]["f1"] * 100,
+    axis=1,
+)
